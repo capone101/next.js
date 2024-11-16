46272ded1e36268be43ac055c6d4781211dd3adf
capone101 <123740021+capone101@users.noreply.github.com>
2024-11-16 20:31:51 +0100

    Update nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -23,8 +23,8 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-return (
-<>
+  return (
+    <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
@@ -35,9 +35,8 @@ return (
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathname === link.href,
-              },
+              }
             )}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
@@ -47,3 +46,4 @@ return (
     </>
   );
 }
+
