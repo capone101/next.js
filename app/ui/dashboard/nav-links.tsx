5c8e2c97088dcfeadd4d763bbcd549c4fac0209c
capone101 <123740021+capone101@users.noreply.github.com>
2024-11-16 20:40:39 +0100

    Update nav-links.tsx

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -31,6 +31,7 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
+            // Only one `className` attribute here
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
@@ -46,4 +47,3 @@ export default function NavLinks() {
     </>
   );
 }
-
